63f47ada6083488f3bc7e2bda08537dcec2cd1e7
Krzysio <krzysztof.kozlowski.1992@gmail.com>
2019-08-03 13:34:56 +0200

    refractoring, colorPicker, stores

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,10 +1,25 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import App from 'view/App';
+import createStores, { StoreMap } from 'stores';
+import initMap, { Api } from 'core';
+
+/******************************/
+
+const appStores: StoreMap = createStores();
+const appMainMapCanvas: HTMLCanvasElement = document.createElement('canvas');
+const appMainMap: Api = initMap(appMainMapCanvas);
+const appView: React.ReactElement = React.createElement(
+    App,
+    {
+        stores: appStores,
+        mainMap: appMainMap
+    }
+);
 
 /******************************/
 
 ReactDom.render(
-    React.createElement(App),
+    appView,
     window.document.getElementById('root')
 );
