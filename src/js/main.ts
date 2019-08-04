0a32002bfe53e274b9103e34e6026299d21ae12c
Krzysio <krzysztof.kozlowski.1992@gmail.com>
2019-08-04 23:09:32 +0200

    storage, ustawienia globalne kolorow mapy, pozycjonowanie okienek do kursora

--- src/js/main.ts
+++ src/js/main.ts
@@ -3,17 +3,20 @@ import * as ReactDom from 'react-dom';
 import App from 'view/App';
 import createStores, { StoreMap } from 'stores';
 import initMap, { Api } from 'core';
+import ModalManager from 'libs/ModalManager';
 
 /******************************/
 
 const appStores: StoreMap = createStores();
 const appMainMapCanvas: HTMLCanvasElement = document.createElement('canvas');
 const appMainMap: Api = initMap(appMainMapCanvas);
+const modalSystem: ModalManager = new ModalManager();
 const appView: React.ReactElement = React.createElement(
     App,
     {
         stores: appStores,
-        mainMap: appMainMap
+        mainMap: appMainMap,
+        modalSystem: modalSystem
     }
 );
 
