f347b7ae095418487bedfbea4222ab36b20f208a
Krzysio <krzysztof.kozlowski.1992@gmail.com>
2019-08-05 22:27:38 +0200

    refractoring, hover, store optimalization

--- src/js/main.ts
+++ src/js/main.ts
@@ -5,11 +5,54 @@ import createStores, { StoreMap } from 'stores';
 import initMap, { Api } from 'core';
 import ModalManager from 'libs/ModalManager';
 
+import { map_key2color } from 'data/mapColors';
+
 /******************************/
 
-const appStores: StoreMap = createStores();
+// main modules
 const appMainMapCanvas: HTMLCanvasElement = document.createElement('canvas');
 const appMainMap: Api = initMap(appMainMapCanvas);
+const appStores: StoreMap = createStores();
+
+// bind stores & mainMap
+(function(stores, mainMap){
+    let globalStore = stores.global,
+        previosGlobalState = globalStore.getState();
+
+    let unsubscriber = globalStore.subscribe(() => {
+        let newGlobalState = globalStore.getState(),
+            flagChanged: boolean;
+
+        flagChanged = false;
+        for (let key in map_key2color) {
+            let newValue = newGlobalState[ `map_${ key }` ],
+                oldValue = previosGlobalState[ `map_${ key }` ];
+
+            if (newValue !== oldValue) {
+                flagChanged = true;
+            }
+        }
+
+        if (flagChanged) {
+            // update colors map
+            for (let key in map_key2color) {
+                let newValue = newGlobalState[ `map_${ key }` ];
+
+                mainMap.buffersManager.setColor(
+                    map_key2color[ key ].index,
+                    newValue
+                );
+            }
+            mainMap.renderer.updateColors(
+                mainMap.buffersManager.getColorBuffer()
+            );
+        }
+
+        previosGlobalState = newGlobalState;
+    });
+})(appStores, appMainMap);
+
+// view
 const modalSystem: ModalManager = new ModalManager();
 const appView: React.ReactElement = React.createElement(
     App,
