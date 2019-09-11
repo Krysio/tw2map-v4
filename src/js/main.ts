f4b65b34932b773c4e116b12293a04e3970a4dc6
Krzysio <krzysztof.kozlowski.1992@gmail.com>
2019-09-11 23:59:51 +0200

    refractoring, binding modules

--- src/js/main.ts
+++ src/js/main.ts
@@ -4,28 +4,30 @@ import debug from 'debug';
 
 import initData from 'data';
 import initCanvas, { CanvasApi } from 'canvas';
-import createStores, { StoreMap } from 'stores';
+import createStores from 'stores';
 
 import App from 'view/App';
 import ModalManager from 'libs/ModalManager';
 
-import { map_key2color } from 'canvas/data/mapColors';
-
 /******************************/
+
+// debug logging
 
 if (process.env.NODE_ENV === 'development') {
     debug.enable('app,app:*');
 }
+const debugLog = debug('app');
 
 /******************************/
 
-const debugLog = debug('app');
-
 // main modules
+
 const appData = initData();
 const appMainCanvasElemetn: HTMLCanvasElement = document.createElement('canvas');
 const appMainCanvas: CanvasApi = initCanvas(appMainCanvasElemetn, appData);
-const appStores: StoreMap = createStores();
+const appStores = createStores();
+
+// TODO load data
 
 (async function(){
     await appMainCanvas.initPromise;
@@ -55,71 +57,21 @@ const appStores: StoreMap = createStores();
 
 /******************************/
 
-// bind stores & mainMap
-(function(stores, mainMapEvents){
-    let globalStore = stores.global,
-        previosGlobalState = globalStore.getState();
+// binding
 
-    // set colors to canvas on change
+import bindMainCanvasGlobalStore from 'bind/mainCanvas-globalStore';
+import bindMainCanvasWorldStore from 'bind/mainCanvas-worldStore';
+import bindLocalStorageGlobalStore from 'bind/localStorage-globalStore';
+import bindLocalStorageWorldStore from 'bind/localStorage-worldStore';
 
-    let unsubscriber = globalStore.subscribe(() => {
-        let newGlobalState = globalStore.getState();
-
-        if (newGlobalState['map'] !== previosGlobalState['map']) {
-            // update colors map
-            for (let key in map_key2color) {
-                let color = newGlobalState['map'][ key ];
-
-                appMainCanvas.commands.setColorInSlot(
-                    map_key2color[ key ].index,
-                    color
-                );
-            }
-        }
-
-        previosGlobalState = newGlobalState;
-    });
-
-    // change store after change canvas view
-
-    let saveViewTimeoutId = null,
-        viewState = {
-            x: 0,
-            y: 0,
-            size: 0
-        },
-        saveView = () => {
-            // TODO
-            console.log('save world store', viewState);
-        },
-        requestSaveView = () => {
-            if (saveViewTimeoutId !== null) {
-                clearTimeout(saveViewTimeoutId);
-            }
-
-            saveViewTimeoutId = setTimeout(saveView, 3e3);
-        };
-
-    mainMapEvents.on('changed position/tile', (position) => {
-        viewState.x = position.x;
-        viewState.y = position.y;
-        requestSaveView();
-    });
-    mainMapEvents.on('changed canvas/size/tile', (value) => {
-        viewState.size = value;
-        requestSaveView();
-    });
-})(appStores, appMainCanvas.events);
-// bind stores with localStorage
-(function(stores){
-    let unsubscriber = stores.global.subscribe(() => {
-        window.localStorage.setItem('global', JSON.stringify(stores.global.getState()));
-    });
-
-    stores.global.actions.importDb();
-})(appStores);
+bindMainCanvasGlobalStore(appMainCanvas, appStores.global);
+bindMainCanvasWorldStore(appMainCanvas, appStores.world);
+bindLocalStorageGlobalStore(appStores.global);
+bindLocalStorageWorldStore(appStores.world);
 
 /******************************/
+
+// view
 
 (function(){
     window.document.getElementById('loader').remove();
